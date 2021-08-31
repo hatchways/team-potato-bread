3f49f232a43ea82149b6df67ecab9395083e1785
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-08-30 22:06:07 -0700

    passing prop and refactor calendar

--- client/src/components/BookingCalendar/BookingCalendar.tsx
+++ client/src/components/BookingCalendar/BookingCalendar.tsx
@@ -1,29 +1,28 @@
-import { useState } from 'react';
 import { Paper, IconButton } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, Calendar } from '@material-ui/pickers';
 import useStyles from './useStyles';
 import { MuiThemeProvider } from '@material-ui/core';
 import { calendarTheme } from './calendarTheme';
-//TODO: pass props and update bookedDate to update current bookedDates if user (sitter)
-const BookingCalendar = (): JSX.Element => {
-  const classes = useStyles();
-  const [bookedDates, setBookedDates] = useState(['8/13/2021', '8/16/2021', '9/16/2021']);
 
+interface DatesProps {
+  dates: string[];
+}
+
+const BookingCalendar: React.FC<DatesProps> = ({ dates }): JSX.Element => {
+  const classes = useStyles();
   function getBookedDayElement([...props]): JSX.Element {
     const day = props[0];
-    const selectedDate = props[1];
     const dayInCurrentMonth = props[2];
-    const date = `${day.getMonth() + 1}/${day.getDate()}/${day.getFullYear()}`;
-    const today = `${selectedDate.getMonth() + 1}/${selectedDate.getDate()}/${selectedDate.getFullYear()}`;
+    const date = day.toGMTString().slice(5, 16).trim().toUpperCase();
     let datesButton;
-    if (bookedDates.indexOf(date) >= 0) {
+    if (dayInCurrentMonth && dates.indexOf(date) >= 0) {
       datesButton = (
         <IconButton className={classes.selectedDate}>
           <span> {day.getDate()} </span>
         </IconButton>
       );
-    } else if (date === today && dayInCurrentMonth) {
+    } else if (dayInCurrentMonth && day.toDateString() === new Date().toDateString()) {
       datesButton = (
         <IconButton className={classes.today}>
           <span> {day.getDate()} </span>
--- client/src/pages/Booking/MyBookings.tsx
+++ client/src/pages/Booking/MyBookings.tsx
@@ -3,18 +3,42 @@ import MgnBookingHeader from '../../components/MgnBookingHeader/MgnBookingHeader
 import BookingCalendar from '../../components/BookingCalendar/BookingCalendar';
 import BookingList from '../../components/BookingList/BookingList';
 import useStyles from './useStyles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import data from './mockData';
+import { User } from '../../interface/User';
+import React from 'react';
 
-export default function MyBookings(): JSX.Element {
+export interface Bookings {
+  id: string;
+  date: string;
+  time: string;
+  username: string;
+  url: string;
+  accepted: boolean;
+  declined: boolean;
+}
+
+const MyBookings: React.FC<User> = (): JSX.Element => {
   const classes = useStyles();
-  const [bookings, setBookings] = useState([]);
+  const [bookings, setBookings] = useState<Bookings[]>();
+  const [dates, setDates] = useState<string[]>();
+
+  useEffect(() => {
+    setBookings(data);
+    const bookingDates = data.map((booking) => {
+      return booking.date.trim();
+    });
+    setDates(bookingDates);
+  }, []);
+
   return (
     <Grid container>
       <MgnBookingHeader />
       <Grid container className={classes.myBooking} spacing={2}>
-        <BookingList />
-        <BookingCalendar />
+        {bookings && <BookingList />}
+        {dates && <BookingCalendar dates={dates} />}
       </Grid>
     </Grid>
   );
-}
+};
+export default MyBookings;
