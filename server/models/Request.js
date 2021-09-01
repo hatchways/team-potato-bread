072e7af81e6d7b0a750c068bea8ded1f0200d1f9
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-09-01 11:41:24 -0700

    change data type to ref user model

--- server/models/Request.js
+++ server/models/Request.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
   userId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   sitterId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   start: {
