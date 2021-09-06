1cdb1e7ac65cfa8f967d386abac652edb9cf2741
Ephraim Hu <mam.nonglaks@gmail.com>
2021-09-06 16:01:47 -0700

    add apiCall for requests

---
+++ client/src/helpers/APICalls/getRequests.ts
@@ -0,0 +1,15 @@
+import { AuthApiData } from '../../interface/AuthApiData';
+import { FetchOptions } from '../../interface/FetchOptions';
+
+const getRequests = async (userId: string): Promise<AuthApiData> => {
+  const fetchOptions: FetchOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+  };
+  return await fetch(`/request?userId=${userId}`, fetchOptions)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
+
+export default getRequests;
