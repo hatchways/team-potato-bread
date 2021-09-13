07291f56cd3b901a957d5324fd6583da1cb55fb0
Your Name <you@example.com>
2021-09-13 11:32:44 -0400

    #3

--- server/routes/request.js
+++ server/routes/request.js
@@ -5,6 +5,8 @@ const {
   getRequests,
   createRequest,
   updateRequest,
+  payRequest,
+
 } = require("../controllers/request");
 
 router.route("/").get(protect, getRequests);
@@ -12,5 +14,8 @@ router.route("/").get(protect, getRequests);
 router.route("/").post(protect, createRequest);
 
 router.route("/").put(protect, updateRequest);
+
+router.route("/:id/pay").post(protect,payRequest);
+
 
 module.exports = router;
