ec3e36a609252f0ec7e4a3fc86cfd5667d675c3a
Ephraim Hu <mam.nonglaks@gmail.com>
2021-09-14 11:26:24 -0700

    add routing path when click notification

--- client/src/components/Notification/NotificationItem/NotificationItem.tsx
+++ client/src/components/Notification/NotificationItem/NotificationItem.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import useStyles from './useStyles';
 import { Box, Avatar, Badge, Typography, ThemeProvider, Paper, createMuiTheme } from '@material-ui/core';
-
+import { Link } from 'react-router-dom';
 const NotificationItem = (): JSX.Element => {
   const classes = useStyles();
+  const [reRoute, setReRoute] = useState<string>('');
+
+  const handleClick = () => {
+    //if booking type link booking > rerout to booking page
+    setReRoute('mybookings');
+  };
 
   return (
-    <Box className={classes.mainBox}>
+    <Box className={classes.mainBox} onClick={handleClick}>
       <Avatar
         className={classes.ownerAvatar}
         variant="square"
@@ -14,11 +20,13 @@ const NotificationItem = (): JSX.Element => {
         alt="ownername"
         src="/static/images/avatar/1.jpg"
       />
-      <Box className={classes.infoBox}>
-        <Typography className={classes.description}>Marry has requested your service for 2 hurs</Typography>
-        <Typography variant="caption">Dog Sitting</Typography>
-        <Typography className={classes.date}>{new Date().toLocaleDateString()}</Typography>
-      </Box>
+      <Link to={`/${reRoute}`}>
+        <Box className={classes.infoBox}>
+          <Typography className={classes.description}>Marry has requested your service for 2 hurs</Typography>
+          <Typography variant="caption">Dog Sitting</Typography>
+          <Typography className={classes.date}>{new Date().toLocaleDateString()}</Typography>
+        </Box>
+      </Link>
     </Box>
   );
 };
