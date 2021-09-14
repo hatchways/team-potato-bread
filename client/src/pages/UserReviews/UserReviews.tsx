1ef11608d1eaf1d3957d990cf14944ffedce9970
mthanki <60077334+mthanki@users.noreply.github.com>
2021-09-14 02:45:59 -0400

    review listing

---
+++ client/src/pages/UserReviews/UserReviews.tsx
@@ -0,0 +1,36 @@
+import { Avatar, Card, CardContent, CardHeader, CssBaseline, Grid, IconButton, Typography } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Rating } from '@material-ui/lab';
+import useStyles from './useStyles';
+
+export default function UserReviews(): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <Grid xs={11} sm={10} md={8} container className={classes.root}>
+      <CssBaseline />
+      <Grid className={classes.reviewsWrapper}>
+        <Grid item>User section</Grid>
+        <Card className={classes.reviewCard}>
+          <CardHeader
+            avatar={<Avatar aria-label="recipe">R</Avatar>}
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+          />
+          <CardContent>
+            <Rating />
+            <Typography variant="body2" color="textSecondary" component="p">
+              This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup
+              of frozen peas along with the mussels, if you like.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
