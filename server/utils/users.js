9c091e7c7cd5edac3537ee6b883ce7a2d852e8b9
Your Name <you@example.com>
2021-09-23 17:36:38 -0400

    Chat UI and IN

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,6 +1,6 @@
 const Conversation = require('../models/Conversation');
 const users = [];
-const addUser = async ({ id, userId ,conversationId}) => {
+const addUser = async ({ id, userId, conversationId }) => {
   const conversation = await Conversation.findById({ _id: conversationId });
 
   if (
@@ -27,4 +27,33 @@ const getUser = (id) => users.find((user) => user.id === id);
 const getUsersInRoom = (conversationId) =>
   users.filter((user) => user.conversationId === conversationId);
 
-module.exports = { addUser, removeUser, getUser, getUsersInRoom, };
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };const Conversation = require('../models/Conversation');
+const users = [];
+const addUser = async ({ id, userId, conversationId }) => {
+  const conversation = await Conversation.findById({ _id: conversationId });
+
+  if (
+    conversation.recieverId.equals(userId) ||
+    conversation.senderId.equals(userId)
+  ) {
+    const user = { id, userId, conversationId };
+
+    users.push(user);
+
+    return { user };
+  }
+
+  return { error: 'Wrong userProfileId' };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index > -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (conversationId) =>
+  users.filter((user) => user.conversationId === conversationId);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
