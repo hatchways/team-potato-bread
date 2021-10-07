521a7da1a768ef9a061ba8cfa24f7a78b82d82d8
Your Name <you@example.com>
2021-10-06 23:00:58 -0400

    added pet model and controller

---
+++ server/models/Pet.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const petSchema = new mongoose.Schema(
+  {
+    name: String,
+    breed: String,
+    age: Number,
+    weight: Number,
+    status: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'status',
+      },
+    ],
+    petPhoto: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'image',
+    },
+    description: String,
+    sex: String,
+    spayedOrNeutered: Boolean,
+    FeedingSchedule: String,
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'profile',
+    },
+    photoGallery: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'image',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Pet = mongoose.model('pet', petSchema);
